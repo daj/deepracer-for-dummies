cd8b99071863703126f1436160e10bd9f378b731
Dan Jarvis <daj@users.noreply.github.com>
2019-11-16 12:46:13 -0500

    Fix (probably benign) warning produced by stop.sh
    ```
    $ ./stop.sh
    WARNING: The ROBOMAKER_COMMAND variable is not set. Defaulting to a blank string.
    ...
    ```

--- scripts/training/stop.sh
+++ scripts/training/stop.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# docker-compose.yml uses "${ROBOMAKER_COMMAND}", so that environment
+# variable needs to be available otherwise Docker outputs a (presumably
+# benign) warning.  Doing an export in a shell script only applies 
+# to the commands run from that script, so the export in start.sh does
+# not propagate to here.
+export ROBOMAKER_COMMAND="./run.sh build distributed_training.launch"
 docker-compose -f ../../docker/docker-compose.yml down
 
 docker stop $(docker ps | awk ' /sagemaker/ { print $1 }')
